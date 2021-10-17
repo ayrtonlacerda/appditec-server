1fc6e9c5f975ea8c5e433da1cbce37c02022abaf
Ayrton <ayrtonlacerda.work@gmail.com>
2021-10-17 20:52:52 -0300

    remove middleware auth

--- api/index.js
+++ api/index.js
@@ -14,13 +14,13 @@ app.get('/helloworld', async (req, res) => {
   return res.status(200).json({ m: 'Hello World' })
 })
 app.post('/auth/login', user.Login)
-
+/* 
 app.use((req, res, next) => {
   const { authorization } = req.headers
   if (authorization !== 'Bearer myToken') return res.status(401).json({ error: 'token invalido' })
   next();
 })
-
+ */
 
 // nao original
 // traz os vestigios de acordo com o usuario e o tipo de pericia ?
