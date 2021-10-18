8b41b08839af44f663ccf0ac26d0f2410bb589d2
Ayrton <ayrtonlacerda.work@gmail.com>
2021-10-17 21:19:57 -0300

    rrrr

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-
+/*
 // controllers
 const user = require('./controller/user')
 const vestigios = require('./controller/vestigios')
@@ -9,24 +9,24 @@ const pops = require('./controller/pops')
 // app
 // trigger
 
-app.use(express.json());
+app.use(express.json()); */
 app.get('/helloworld', async (req, res) => {
   return res.status(200).json({ m: 'Hello World' })
 })
-app.post('/auth/login', user.Login)
-/* 
+/* app.post('/auth/login', user.Login)
+
 app.use((req, res, next) => {
   const { authorization } = req.headers
   if (authorization !== 'Bearer myToken') return res.status(401).json({ error: 'token invalido' })
   next();
 })
- */
+ 
 
 // nao original
 // traz os vestigios de acordo com o usuario e o tipo de pericia ?
 app.get('/vestigio', vestigios.index)
 
-app.get('/dowloadpop', pops.index)
+app.get('/dowloadpop', pops.index)*/
 
 
 module.exports = app
